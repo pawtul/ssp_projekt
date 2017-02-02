f7d800afacd8d8134d30ff1154cf453a46b9cea7
tulowiecki <pawtul@gmail.com>
2017-02-03 00:12:35 +0100

    added reverse package handling

--- src/main/java/pl/edu/agh/kt/SdnLabListener.java
+++ src/main/java/pl/edu/agh/kt/SdnLabListener.java
@@ -7,6 +7,8 @@ import java.util.Random;
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFPacketIn;
 import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.IPv4Address;
 import org.projectfloodlight.openflow.types.OFPort;
 
 import net.floodlightcontroller.core.FloodlightContext;
@@ -16,9 +18,12 @@ import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
-
+import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.IPv4;
 import net.floodlightcontroller.core.IFloodlightProviderService;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,6 +53,11 @@ public class SdnLabListener implements IFloodlightModule, IOFMessageListener {
 	@Override
 	public net.floodlightcontroller.core.IListener.Command receive(IOFSwitch sw, OFMessage msg,
 			FloodlightContext cntx) {
+		Map<IPv4Address, Integer> Hosts = new HashMap();
+		Hosts.put(IPv4Address.of("10.0.0.3"), 3);
+		Hosts.put(IPv4Address.of("10.0.0.4"), 4);
+		Hosts.put(IPv4Address.of("10.0.0.5"), 5);
+		Hosts.put(IPv4Address.of("10.0.0.6"), 6);
 
 		logger.info("************* NEW PACKET IN *************");
 		// PacketExtractor extractor = new PacketExtractor();
@@ -56,15 +66,26 @@ public class SdnLabListener implements IFloodlightModule, IOFMessageListener {
 		// sending PacketOut
 		OFPacketIn pin = (OFPacketIn) msg;
 		OFPort outPort = OFPort.of(0);
-
 		// rozdzielanie ruchu
-		outPort = OFPort.of(this.dispatch());
+		if (pin.getInPort() == OFPort.of(3) || pin.getInPort() == OFPort.of(4) || pin.getInPort() == OFPort.of(5)
+				|| pin.getInPort() == OFPort.of(6)) {
+			outPort = OFPort.of(this.dispatch());
+		}
+		else{
+			Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+			if (eth.getEtherType() == EthType.IPv4){
+				IPv4 ip = (IPv4) eth.getPayload();
+				IPv4Address dstIp = ip.getDestinationAddress();
+				outPort = OFPort.of(Hosts.get(dstIp));
+			}
+			return Command.CONTINUE;
+		}
 
 		// if (pin.getInPort() == OFPort.of(1)) {
 		// outPort = OFPort.of(2);
 		// } else
 		// outPort = OFPort.of(1);
-
+//
 		Flows.simpleAdd(sw, pin, cntx, outPort);
 		Flows.sendPacketOut(sw);
 
@@ -79,10 +100,11 @@ public class SdnLabListener implements IFloodlightModule, IOFMessageListener {
 		Random rn = new Random();
 		// max to 100
 		int max = 100;
-		int port_1 = rn.nextInt(0) % max;
-		int port_2 = rn.nextInt(0) % max;
-		int outPort = -1;
-		if (port_1 <= port_2)
+		int load1 = rn.nextInt(100) % max;
+		int load2 = rn.nextInt(100) % max;
+		logger.info("Load1 is equal {}% load2 is equal {}%",load1, load2);
+		int outPort = 1;
+		if (load1 <= load2)
 			outPort = 1;
 		else
 			outPort = 2;
