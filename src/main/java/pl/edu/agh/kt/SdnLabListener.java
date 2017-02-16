738cccfb45294840eaae514d52f7f07ce474e17a
tulowiecki <pawtul@gmail.com>
2017-02-16 21:03:08 +0100

    Basic ARP

--- src/main/java/pl/edu/agh/kt/SdnLabListener.java
+++ src/main/java/pl/edu/agh/kt/SdnLabListener.java
@@ -9,6 +9,8 @@ import org.projectfloodlight.openflow.protocol.OFPacketIn;
 import org.projectfloodlight.openflow.protocol.OFType;
 import org.projectfloodlight.openflow.types.EthType;
 import org.projectfloodlight.openflow.types.IPv4Address;
+import org.projectfloodlight.openflow.types.IpDscp;
+import org.projectfloodlight.openflow.types.MacAddress;
 import org.projectfloodlight.openflow.types.OFPort;
 
 import net.floodlightcontroller.core.FloodlightContext;
@@ -18,7 +20,9 @@ import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.packet.ARP;
 import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.IPacket;
 import net.floodlightcontroller.packet.IPv4;
 import net.floodlightcontroller.core.IFloodlightProviderService;
 import java.util.ArrayList;
@@ -54,10 +58,21 @@ public class SdnLabListener implements IFloodlightModule, IOFMessageListener {
 	public net.floodlightcontroller.core.IListener.Command receive(IOFSwitch sw, OFMessage msg,
 			FloodlightContext cntx) {
 		Map<IPv4Address, Integer> Hosts = new HashMap();
+		Hosts.put(IPv4Address.of("10.0.0.1"), 1);
+		Hosts.put(IPv4Address.of("10.0.0.2"), 2);
 		Hosts.put(IPv4Address.of("10.0.0.3"), 3);
 		Hosts.put(IPv4Address.of("10.0.0.4"), 4);
 		Hosts.put(IPv4Address.of("10.0.0.5"), 5);
 		Hosts.put(IPv4Address.of("10.0.0.6"), 6);
+		
+		Map<IPv4Address, String> MACs = new HashMap();
+		MACs.put(IPv4Address.of("10.0.0.1"), "00:00:00:00:00:01");
+		MACs.put(IPv4Address.of("10.0.0.2"), "00:00:00:00:00:02");
+		MACs.put(IPv4Address.of("10.0.0.3"), "00:00:00:00:00:03");
+		MACs.put(IPv4Address.of("10.0.0.4"), "00:00:00:00:00:04");
+		MACs.put(IPv4Address.of("10.0.0.5"), "00:00:00:00:00:05");
+		MACs.put(IPv4Address.of("10.0.0.6"), "00:00:00:00:00:06");
+		// TODO: dodac adresy MAC
 
 		logger.info("************* NEW PACKET IN *************");
 		// PacketExtractor extractor = new PacketExtractor();
@@ -67,36 +82,48 @@ public class SdnLabListener implements IFloodlightModule, IOFMessageListener {
 		OFPacketIn pin = (OFPacketIn) msg;
 		OFPort outPort = OFPort.of(0);
 		// // rozdzielanie ruchu
+		// TODO rozdzielis na ARP i IPv4
 		Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-		if (pin.getInPort() == OFPort.of(3) || pin.getInPort() == OFPort.of(4) || pin.getInPort() == OFPort.of(5)
-				|| pin.getInPort() == OFPort.of(6)) {
-			outPort = OFPort.of(this.dispatch());
-			Flows.simpleAdd(sw, pin, cntx, outPort);
-			Flows.sendPacketOut(sw);
-			return Command.STOP;
-		} else {
+//		if (pin.getInPort() == OFPort.of(3) || pin.getInPort() == OFPort.of(4) || pin.getInPort() == OFPort.of(5)
+//				|| pin.getInPort() == OFPort.of(6)) {
+//			outPort = OFPort.of(this.dispatch());
+//			final IPv4 iPv4Packet = (IPv4) eth.getPayload();
+//            final IPv4Address iPv4DestinationAddress = iPv4Packet.getDestinationAddress();
+//			Flows.simpleAdd(sw, pin, cntx, outPort);
+//			Flows.sendPacketOut(sw);
+//			return Command.STOP;
+//		} else {
 //			IPv4 ip = (IPv4) eth.getPayload();
 //			IPv4Address dstIp = ip.getDestinationAddress();
 //			outPort = OFPort.of(Hosts.get(dstIp));
 //			Flows.simpleAdd(sw, pin, cntx, outPort);
 //			Flows.sendPacketOut(sw);
 //			return Command.STOP;
-			return Command.CONTINUE;
-		}
+//			return Command.CONTINUE;
+//		}
+		if (eth.getEtherType() == EthType.IPv4) {
+			logger.info("got IPv4");
+            IPv4 ipv4 = (IPv4) eth.getPayload();
+             
+            /* More to come here */
+        } else if (eth.getEtherType() == EthType.ARP) {
+        	logger.info("got ARP");
+            ARP arp = (ARP) eth.getPayload();
+            IPv4Address senderAddress = arp.getSenderProtocolAddress();
+            MacAddress senderMAC = arp.getSenderHardwareAddress();
+            IPv4Address targetAddress = arp.getTargetProtocolAddress();
+//            MacAddress targetMAC = arp.getTargetHardwareAddress(); <- target address to 00:00:00:00:00:00
+//            logger.info(senderMAC.toString());
+            logger.info(MACs.get(targetAddress));
+  
+            /* More to come here */
+  
+        }
+        return Command.CONTINUE;
 
-		// if (pin.getInPort() == OFPort.of(1)) {
-		// outPort = OFPort.of(2);
-		// } else
-		// outPort = OFPort.of(1);
-		//
-		// Flows.simpleAdd(sw, pin, cntx, outPort);
-		// Flows.sendPacketOut(sw);
-
-		// return Command.STOP;
-//		return Command.CONTINUE;
 	}
 
-	public int dispatch() {
+	public IPv4Address dispatch() {
 		/*
 		 * wybiera na jaki port przescac pakiet
 		 */
@@ -106,11 +133,11 @@ public class SdnLabListener implements IFloodlightModule, IOFMessageListener {
 		int load1 = rn.nextInt(100) % max;
 		int load2 = rn.nextInt(100) % max;
 		logger.info("Load1 is equal {}% load2 is equal {}%", load1, load2);
-		int outPort = 1;
+		IPv4Address outPort;
 		if (load1 <= load2)
-			outPort = 1;
+			outPort = IPv4Address.of("10.0.0.1");
 		else
-			outPort = 2;
+			outPort = IPv4Address.of("10.0.0.2");
 		return outPort;
 	}
 
